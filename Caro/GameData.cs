f42967caeac76688c451d60c94dfdbe7f4a1da65
Az3r <tuan99kghd@gmail.com>
2019-11-12 14:30:06 +0700

    Add project files.

---
+++ Caro/GameData.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+namespace Caro
+{
+    [Serializable]
+    [XmlRoot()]
+    public class GameData : ISerializable
+    {
+        public GameData() { }
+        /*
+         * Helpers
+         */
+        private void Log(string message = "", [CallerMemberName] string tag = "")
+        {
+            Debug.WriteLine($"@{tag}: {message}");
+        }
+        private void LogIf(bool condition, string message = "", [CallerMemberName] string tag = "")
+        {
+            Debug.WriteLineIf(condition, $"@{tag}: {message}");
+        }
+
+        private Player CheckHorizon(Point grid)
+        {
+            Log($"checking move [{grid.X}, {grid.Y}]");
+
+            /*
+             note: OPlayerMove = 1, XPlayerMove = -1
+             imagine if a grid has a value of (1,-1) will mean that it is owned by O-Player,
+             and a grid with a value of (1,-1) is owned by X-Player
+             winning move is decided by calculating the accumulated sum of all horizontal grids,
+             if the sum is (WinPoint, -WinPoint), O-Player wins, and with (-WinPoint, WinPoint) means X-Player wins
+            */
+
+            Point sum = new Point();
+            for (int x = 0; x < MapSize; ++x)
+            {
+                int playerID = Map[x, (int)grid.Y];
+                int n = 0;
+                if (playerID == OPlayer.ID) n = 1;
+                if (playerID == XPlayer.ID) n = -1;
+                sum.X += n;
+                sum.Y += -n;
+            }
+            double absO = Math.Abs(sum.X);
+            double absX = Math.Abs(sum.Y);
+            LogIf(absX == WinPoint && absO == WinPoint, $"[{grid.X}, {grid.Y}] is winning move");
+            if (Math.Abs(sum.X) == WinPoint) return OPlayer;
+            if (Math.Abs(sum.Y) == -WinPoint) return XPlayer;
+            return null;
+        }
+        private Player CheckVertical(Point grid)
+        {
+            Log($"checking move [{grid.X}, {grid.Y}]");
+
+            /*
+             note: OPlayerMove = 1, XPlayerMove = -1
+             imagine if a grid has a value of (1,-1) will mean that it is owned by O-Player,
+             and a grid with a value of (1,-1) is owned by X-Player
+             winning move is decided by calculating the accumulated sum of all horizontal grids,
+             if the sum is (WinPoint, -WinPoint), O-Player wins, and with (-WinPoint, WinPoint) means X-Player wins
+            */
+
+            Point sum = new Point();
+            for (int y = 0; y < MapSize; ++y)
+            {
+                int playerID = Map[(int)grid.X, y];
+                int n = 0;
+                if (playerID == OPlayer.ID) n = 1;
+                if (playerID == XPlayer.ID) n = -1;
+                sum.X += n;
+                sum.Y += -n;
+            }
+            double absO = Math.Abs(sum.X);
+            double absX = Math.Abs(sum.Y);
+            LogIf(absX == WinPoint && absO == WinPoint, $"[{grid.X}, {grid.Y}] is winning move");
+            if (Math.Abs(sum.X) == WinPoint) return OPlayer;
+            if (Math.Abs(sum.Y) == -WinPoint) return XPlayer;
+            return null;
+        }
+        private Player CheckDiagonal(Point grid)
+        {
+            Log($"checking move [{grid.X}, {grid.Y}]");
+
+            /*
+             note: OPlayerMove = 1, XPlayerMove = -1
+             imagine if a grid has a value of (1,-1) will mean that it is owned by O-Player,
+             and a grid with a value of (1,-1) is owned by X-Player
+             winning move is decided by calculating the accumulated sum of all horizontal grids,
+             if the sum is (WinPoint, -WinPoint), O-Player wins, and with (-WinPoint, WinPoint) means X-Player wins
+            */
+
+            Point sum = new Point();
+            double diagPos = grid.X - grid.Y;
+            if (diagPos < 0)
+            {
+                for (int y = (int)-diagPos; y < MapSize; ++y)
+                {
+                    int playerID = Map[(int)(y + diagPos), y];
+                    int n = 0;
+                    if (playerID == OPlayer.ID) n = 1;
+                    if (playerID == XPlayer.ID) n = -1;
+                    sum.X += n;
+                    sum.Y += -n;
+                }
+            }
+            else
+            {
+                for (int x = (int)diagPos; x < MapSize; ++x)
+                {
+                    int playerID = Map[x, (int)(x - diagPos)];
+                    int n = 0;
+                    if (playerID == OPlayer.ID) n = 1;
+                    if (playerID == XPlayer.ID) n = -1;
+                    sum.X += n;
+                    sum.Y += -n;
+                }
+            }
+
+            double absO = Math.Abs(sum.X);
+            double absX = Math.Abs(sum.Y);
+            LogIf(absX == WinPoint && absO == WinPoint, $"[{grid.X}, {grid.Y}] is winning move");
+            if (Math.Abs(sum.X) == WinPoint) return OPlayer;
+            if (Math.Abs(sum.Y) == -WinPoint) return XPlayer;
+            return null;
+        }
+        private Player CheckAntiDiagonal(Point grid)
+        {
+            Log($"checking move [{grid.X}, {grid.Y}]");
+
+            /*
+             note: OPlayerMove = 1, XPlayerMove = -1
+             imagine if a grid has a value of (1,-1) will mean that it is owned by O-Player,
+             and a grid with a value of (1,-1) is owned by X-Player
+             winning move is decided by calculating the accumulated sum of all horizontal grids,
+             if the sum is (WinPoint, -WinPoint), O-Player wins, and with (-WinPoint, WinPoint) means X-Player wins
+            */
+
+            Point sum = new Point();
+            double antiDiagPos = grid.X + grid.Y;
+            if (antiDiagPos < MapSize)
+            {
+                for (int x = 0; x <= antiDiagPos; ++x)
+                {
+                    int playerID = Map[x, (int)(antiDiagPos - x)];
+                    int n = 0;
+                    if (playerID == OPlayer.ID) n = 1;
+                    if (playerID == XPlayer.ID) n = -1;
+                    sum.X += n;
+                    sum.Y += -n;
+                }
+            }
+            else
+            {
+                for (int y = (int)antiDiagPos - (MapSize - 1); y < MapSize; ++y)
+                {
+                    int playerID = Map[(int)(antiDiagPos - y), y];
+                    int n = 0;
+                    if (playerID == OPlayer.ID) n = 1;
+                    if (playerID == XPlayer.ID) n = -1;
+                    sum.X += n;
+                    sum.Y += -n;
+                }
+            }
+
+            double absO = Math.Abs(sum.X);
+            double absX = Math.Abs(sum.Y);
+            LogIf(absX == WinPoint && absO == WinPoint, $"[{grid.X}, {grid.Y}] is winning move");
+            if (Math.Abs(sum.X) == WinPoint) return OPlayer;
+            if (Math.Abs(sum.Y) == -WinPoint) return XPlayer;
+            return null;
+        }
+
+        /*
+         * Interface
+         */
+
+        public int Initialize()
+        {
+            Map = new int[MapSize, MapSize];
+            Trace.WriteLine($"Initializing...");
+            Trace.WriteLine($"Map's size: {MapSize}");
+            Trace.WriteLine($"X-Player: id={XPlayer.ID}, name={XPlayer.Name}");
+            Trace.WriteLine($"O-Player: id={OPlayer.ID}, name={OPlayer.Name}");
+
+            Trace.WriteLineIf(MoveLogger.Count > 0, "Detected that game has been loaded");
+            if (MoveLogger.Count > 0)
+            {
+                // get the first player to move
+                PlayerToMove = MoveLogger[0].Player;
+                int turn = 0;
+                foreach (GameMove item in MoveLogger)
+                {
+                    Point move = item.Move;
+                    int id = item.Player.ID;
+                    Trace.WriteLine($"Turn {turn}: Player {item.Player.Name} has moved [{move.X}, {move.Y}]");
+                    Map[(int)move.X, (int)move.Y] = id;
+
+                    if (++PlayMovedCount >= 2)
+                    {
+                        PlayMovedCount = 0;
+                        ++turn;
+                    }
+
+                    // switch player
+                    if (PlayerToMove.ID == OPlayer.ID) PlayerToMove = XPlayer;
+                    else PlayerToMove = OPlayer;
+                }
+                Trace.Assert(turn == Turn, $"Data mismatch detected: {nameof(Turn)}");
+            }
+
+            if (PlayerToMove == null)
+            {
+                Trace.WriteLine($"{nameof(PlayerToMove)} is not set");
+                return -1;
+            }
+            else Trace.WriteLine($"First to move: id={PlayerToMove.ID}, name={PlayerToMove.Name}");
+            return 0;
+        }
+
+        public int Reset()
+        {
+            MoveLogger.Clear();
+            Map = new int[MapSize, MapSize];
+            Turn = 0;
+            return 0;
+        }
+
+        public int Update(Point move)
+        {
+            Map[(int)move.X, (int)move.Y] = PlayerToMove.ID;
+            // Add to logger
+            MoveLogger.Add(new GameMove(PlayerToMove, move, Turn));
+            Trace.WriteLine($"Turn {Turn}: {PlayerToMove.Name} moved [{move.X}, {move.Y}]");
+            if (++PlayMovedCount >= 2)
+            {
+                PlayMovedCount = 0;
+                ++Turn;
+            }
+            // switch player
+            if (PlayerToMove.ID == OPlayer.ID) PlayerToMove = XPlayer;
+            else PlayerToMove = OPlayer;
+            return 0;
+        }
+
+        public GameMove? IsOver()
+        {
+            Point move = MoveLogger[MoveLogger.Count - 1].Move;
+            if (CheckHorizon(move) != null) return MoveLogger[MoveLogger.Count - 1];
+            if (CheckVertical(move) != null) return MoveLogger[MoveLogger.Count - 1];
+            if (CheckDiagonal(move) != null) return MoveLogger[MoveLogger.Count - 1];
+            if (CheckAntiDiagonal(move) != null) return MoveLogger[MoveLogger.Count - 1];
+            return null;
+        }
+        public bool HasEmptyGrid()
+        {
+            return MoveLogger.Count < MapSize * MapSize;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException(nameof(GetObjectData));
+        }
+
+        public Player XPlayer { get; set; } = new Player(1, "X-Player");
+        public Player OPlayer { get; set; } = new Player(2, "O-Player");
+        public int MapSize { get; set; } = 6;
+        public int WinPoint { get; set; } = 5;
+        public int Turn { get; set; } = 0;
+        public List<GameMove> MoveLogger { get; set; } = new List<GameMove>();
+
+        [XmlIgnore]
+        public Player PlayerToMove { get; set; }
+        [XmlIgnore]
+        public int[,] Map { get; private set; }
+        private int PlayMovedCount = 0;
+    }
+
+    [Serializable]
+    public class Player
+    {
+        public Player()
+        {
+            ID = 0;
+            Name = string.Empty;
+        }
+        public Player(int id, string name)
+        {
+            if (string.IsNullOrEmpty(name)) name = "Player";
+            this.ID = id;
+            this.Name = name;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    [Serializable]
+    public struct GameMove
+    {
+        public GameMove(Player player, Point grid, int gameTurn)
+        {
+            this.Player = player;
+            this.Move = grid;
+            this.GameTurn = gameTurn;
+        }
+        public Player Player { get; set; }
+        public Point Move { get; set; }
+        public int GameTurn { get; set; }
+    }
+}
---
+++ Caro/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.Win32;
+namespace Caro
+{
+    struct SizeInt
+    {
+        public SizeInt(int width,int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public  int Width { get; set; }
+        public  int Height { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameData game = new GameData();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* 
+         * Helpers
+         */
+
+        private Point GetClickedGrid()
+        {
+            Size gridSize = new Size(GameField.ActualWidth / game.MapSize, GameField.ActualHeight / game.MapSize);
+            Point grid = new Point(Math.Floor(MousePosition.X / gridSize.Width), Math.Floor(MousePosition.Y / gridSize.Height));
+            Debug.WriteLine(string.Format("grid [{0}, {1}] is clicked", grid.X, grid.Y));
+            return grid;
+        }
+        private Path CreatePath(int player)
+        {
+            Debug.Assert(player == game.OPlayer.ID || player == game.XPlayer.ID, "invalid path number");
+            if (player == game.OPlayer.ID) return FindResource("O") as Path;
+            else if(player == game.XPlayer.ID) return FindResource("X") as Path;
+            return null;
+        }
+        private void InitializeGame()
+        {
+            // setting up game
+            if (game.PlayerToMove == null) game.PlayerToMove = game.OPlayer;
+            Trace.Assert(game.Initialize() == 0, "Unable to initialize! Check output");
+
+            // setting up UI
+            for (int x = 0; x < game.MapSize; ++x)
+            {
+                for (int y = 0; y < game.MapSize; ++y)
+                {
+                    Button button = new Button
+                    {
+                        Style = FindResource("General") as Style
+                    };
+                    if (game.Map[x, y] != 0) button.Content = CreatePath(game.Map[x, y]);
+                    Grid.SetRow(button, y);
+                    Grid.SetColumn(button, x);
+                    GameField.Children.Add(button);
+                }
+            }
+        }
+
+        /*
+         * Game Events
+         */
+
+
+        private void OnSizeChanging(object sender, MouseWheelEventArgs e)
+        {
+            double factor = 50.0 / 120.0;
+            this.Width += e.Delta * factor;
+            this.Height += e.Delta * factor;
+        }
+
+        private void OnPlayerClick(object sender, RoutedEventArgs e)
+        {
+            Button bt = sender as Button;
+            if (bt.Content == null)
+            {
+                // Update game state
+                bt.Content = CreatePath(game.PlayerToMove.ID);
+                Point clickedGrid = GetClickedGrid();
+                game.Update(clickedGrid);
+                GameMove? winMove = game.IsOver();
+
+                if(winMove.HasValue)
+                {
+                    Player winner = winMove.Value.Player;
+                    Label annoucement = new Label()
+                    {
+                        Style = FindResource("Announcement") as Style,
+                        Content = $"winner: {winner.Name}".ToUpper()
+                    };
+                    Label commands = new Label()
+                    {
+                        Style = FindResource("GameCommand") as Style,
+                        Margin = new Thickness(0.0, 30.0, 0.0, 0.0),
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Content = "Press R to restart game"
+                    };
+
+                    // Update UI
+                    NotifyPanel.Children.Clear();
+                    NotifyPanel.Children.Add(annoucement);
+                    NotifyPanel.Children.Add(commands);
+                    GameNotifyOverlay.Visibility = Visibility.Visible;
+                    GameStatus = GAME_ENDED;
+                }
+                else if (game.HasEmptyGrid() == false)
+                {
+                    // Update UI
+                    Label annoucement = new Label()
+                    {
+                        Style = FindResource("Announcement") as Style,
+                        Content = "game draw".ToUpper()
+                    };
+                    Label commands = new Label()
+                    {
+                        Style = FindResource("GameCommand") as Style,
+                        Margin = new Thickness(0.0, 30.0, 0.0, 0.0),
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Content = "Press R to restart game"
+                    };
+
+                    // Update UI
+                    NotifyPanel.Children.Clear();
+                    NotifyPanel.Children.Add(annoucement);
+                    NotifyPanel.Children.Add(commands);
+                    GameNotifyOverlay.Visibility = Visibility.Visible;
+                    GameStatus = GAME_ENDED;
+                }
+            }
+        }
+        private void MouseTracker(object sender, MouseEventArgs e)
+        {
+            MousePosition = e.GetPosition(this);
+        }
+        private void DisableKeyboardInput(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (GameStatus == 0)
+                {
+                    MenuOverlay.Visibility = Visibility.Hidden;
+                    GameField.Visibility = Visibility.Visible;
+                    InitializeGame();
+                    GameStatus = GAME_RUNNING;
+                }
+                else if (GameStatus == GAME_RUNNING)
+                {
+                    Label pause = new Label()
+                    {
+                        Style = FindResource("Announcement") as Style,
+                        Content = "GAME PAUSED"
+                    };
+                    NotifyPanel.Children.Clear();
+                    NotifyPanel.Children.Add(pause);
+                    GameNotifyOverlay.Visibility = Visibility.Visible;
+                    GameStatus = GAME_PAUSE;
+                }
+                else if (GameStatus == GAME_PAUSE)
+                {
+                    GameNotifyOverlay.Visibility = Visibility.Hidden;
+                    GameStatus = GAME_RUNNING;
+                }
+            }
+            else if (e.Key == Key.Escape)
+            {
+                GameField.Visibility = Visibility.Hidden;
+                GameNotifyOverlay.Visibility = Visibility.Hidden;
+                MenuOverlay.Visibility = Visibility.Visible;
+                game.Reset();
+                GameStatus = 0;
+            }
+            else if (e.Key == Key.R)
+            {
+                game.Reset();
+                GameStatus = GAME_RUNNING;
+                foreach (Button button in GameField.Children)
+                {
+                    button.Content = null;
+                }
+                GameNotifyOverlay.Visibility = Visibility.Hidden;
+                MenuOverlay.Visibility = Visibility.Hidden;
+            }
+            else if (e.Key == Key.F1)
+            {
+                SaveFileDialog dialog = new SaveFileDialog()
+                {
+                    DefaultExt = "caro",
+                    CreatePrompt = true
+                };
+
+                if(dialog.ShowDialog() == true)
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(GameData));
+                    using XmlWriter writer = XmlWriter.Create(dialog.FileName);
+                    serializer.Serialize(writer, game);
+                }
+            }
+            else if (e.Key == Key.F2)
+            {
+                OpenFileDialog dialog = new OpenFileDialog()
+                {
+                    DefaultExt = "caro",
+                    CheckFileExists = true,
+                    Multiselect = false
+                };
+
+                if(dialog.ShowDialog() == true)
+                {
+                    XmlSerializer deserializer = new XmlSerializer(typeof(GameData));
+                    using XmlReader reader = XmlReader.Create(dialog.FileName);
+                    game = deserializer.Deserialize(reader) as GameData;
+                }
+            }
+        }
+        private Point MousePosition;
+        private int GameStatus;
+        private const int GAME_RUNNING = 1;
+        private const int GAME_PAUSE = 2;
+        private const int GAME_ENDED = 3;
+    }
+}
